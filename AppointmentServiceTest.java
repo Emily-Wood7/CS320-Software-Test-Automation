fb535a6cb3c5ffaffbea4435bb9d27af7647f4f1
Emily-Wood7 <95588376+Emily-Wood7@users.noreply.github.com>
2022-10-11 19:27:41 -0400

    Update AppointmentServiceTest.java

---
+++ AppointmentServiceTest.java
@@ -0,0 +1,72 @@
+/*
+ * Emily Wood
+ * CS 320
+ * Module Five Milestone
+ * Ramsey Kraya
+ */
+
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppointmentServiceTest {
+
+    // set up test variables
+    final String passTestId = "0";
+    final String failTestId = "1";
+    final String passTestDate = "12122022";
+    final String passTestDescription = "Appointment Description";
+    AppointmentService appointment;
+
+
+    @BeforeEach
+    // create a new instance for each test
+    void setUp() {
+        appointment = new AppointmentService();
+    }
+
+    @AfterEach
+    // empty the arrayList after each test
+    void tearDown() {
+        AppointmentService.appointmentList.clear();
+    }
+
+    @Test
+    // test an id that exists and one that does not exist
+    void testSearchAppointmentId() {
+        appointment.addAppointment(passTestDate, passTestDescription);
+
+        assertAll(
+                () -> assertEquals(passTestId, appointment.searchAppointmentId(passTestId).getAppointmentId()),
+                () -> assertThrows(IllegalArgumentException.class, () -> appointment.searchAppointmentId(failTestId).getAppointmentId())
+        );
+    }
+
+    @Test
+    // test that each item was added to the appointment
+    void testAddAppointment() {
+        appointment.addAppointment(passTestDate, passTestDescription);
+        assertAll(
+                () -> assertEquals(passTestId, AppointmentService.appointmentList.get(0).getAppointmentId()),
+                () -> assertEquals(passTestDate, AppointmentService.appointmentList.get(0).getAppointmentDate()),
+                () -> assertEquals(passTestDescription, AppointmentService.appointmentList.get(0).getAppointmentDescription())
+        );
+    }
+
+    @Test
+    void testDeleteAppointment() {
+        appointment.addAppointment(passTestDate, passTestDescription);
+
+        int appointmentListSize = AppointmentService.appointmentList.size();
+
+        // delete appointment
+        appointment.deleteAppointment(passTestId);
+
+        // verify appointment was deleted
+        assertEquals(appointmentListSize - 1, AppointmentService.appointmentList.size());
+    }
+}
