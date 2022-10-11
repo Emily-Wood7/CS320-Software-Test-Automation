a1ce225768f06f61ea805e2f31d4c88f598ba7d1
Emily-Wood7 <95588376+Emily-Wood7@users.noreply.github.com>
2022-10-11 19:39:44 -0400

    Update TaskTest.java

---
+++ TaskTest.java
@@ -0,0 +1,77 @@
+/*
+ * Emily Wood
+ * CS 320
+ * Module Five Milestone
+ * Ramsey Kraya
+ */
+
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    final String nullTest = null;
+    final String longIdTest = "01234567890";
+    final String longNameTest = "012345678901234567890";
+    final String longDescriptionTest = "012345678901234567890123456789012345678901234567890";
+    final String goodTest = "Good Test";
+    Task task;
+
+    @BeforeEach
+    // create a new instance before each test
+    public void setUp() {
+        task = new Task();
+    }
+
+    @Test
+    // testing setter and getter good test
+    void testGetTaskId() {
+        task.setTaskId(goodTest);
+        assertTrue(task.getTaskId() == goodTest);
+    }
+
+    @Test
+    // testing setter invalid entries
+    void testSetTaskId() {
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class, () -> task.setTaskId(nullTest)),
+                () -> assertThrows(IllegalArgumentException.class, () -> task.setTaskId(longIdTest))
+        );
+    }
+
+    @Test
+        // testing setter and getter good test
+    void testGetTaskName() {
+        task.setTaskName(goodTest);
+        assertTrue(task.getTaskName() == goodTest);
+    }
+
+    @Test
+        // testing setter invalid entries
+    void testSetTaskName() {
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class, () -> task.setTaskName(nullTest)),
+                () -> assertThrows(IllegalArgumentException.class, () -> task.setTaskName(longNameTest))
+        );
+    }
+
+    @Test
+        // testing setter and getter good test
+    void testGetTaskDescription() {
+        task.setTaskDescription(goodTest);
+        assertTrue(task.getTaskDescription() == goodTest);
+    }
+
+    @Test
+        // testing setter invalid entries
+    void testSetTaskDescription() {
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class, () -> task.setTaskDescription(nullTest)),
+                () -> assertThrows(IllegalArgumentException.class, () -> task.setTaskDescription(longDescriptionTest))
+        );
+    }
+}
